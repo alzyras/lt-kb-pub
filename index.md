fa302fe90ffd31adbf6a9ecd968f797cfc7d0f09
alzyras <tatiminskas@gmail.com>
2026-04-16 19:48:09 +0300

    Configure Quartz GitHub Pages site

---
+++ index.md
@@ -0,0 +1,26 @@
+---
+title: Lietuvos istorijos žinių bazė
+---
+
+# Lietuvos istorijos žinių bazė
+
+Ši svetainė kaupia struktūruotas pastabas iš lietuviškų istorijos šaltinių: žmones, įvykius, vietas, grupes, daiktus, papročius, posakius, žodyno įrašus ir tyrimus.
+
+## Pagrindinės dėžės
+
+- [[objektai/README|Objektai]]
+- [[tyrimai]]
+- [[paveikslėliai]]
+
+## Objektų kategorijos
+
+- [[objektai/asmenys]]
+- [[objektai/autoriai]]
+- [[objektai/ivykiai]]
+- [[objektai/vietos]]
+- [[objektai/grupes]]
+- [[objektai/daiktai]]
+- [[objektai/paprociai]]
+- [[objektai/posakiai]]
+- [[objektai/zodynas]]
+- [[objektai/saltiniai]]
---
+++ quartz.config.ts
@@ -0,0 +1,119 @@
+import { QuartzConfig } from "./quartz/cfg"
+import * as Plugin from "./quartz/plugins"
+
+/**
+ * Quartz 4 Configuration
+ *
+ * See https://quartz.jzhao.xyz/configuration for more information.
+ */
+const config: QuartzConfig = {
+  configuration: {
+    pageTitle: "Lietuvos istorijos žinių bazė",
+    pageTitleSuffix: " – LT KB",
+    enableSPA: true,
+    enablePopovers: true,
+    analytics: null,
+    locale: "lt-LT",
+    baseUrl: "alzyras.github.io/lt-kb-pub",
+    ignorePatterns: [
+      ".git",
+      ".git/**",
+      ".github",
+      ".github/**",
+      ".obsidian",
+      ".obsidian/**",
+      ".quartz-cache",
+      ".quartz-cache/**",
+      "node_modules",
+      "node_modules/**",
+      "public",
+      "public/**",
+      "quartz",
+      "quartz/**",
+      "package.json",
+      "package-lock.json",
+      "tsconfig.json",
+      "globals.d.ts",
+      "index.d.ts",
+      "README.md",
+      "private",
+      "templates",
+    ],
+    defaultDateType: "modified",
+    theme: {
+      fontOrigin: "googleFonts",
+      cdnCaching: true,
+      typography: {
+        title: "Cormorant Garamond",
+        header: "Cormorant Garamond",
+        body: "Source Serif 4",
+        code: "IBM Plex Mono",
+      },
+      colors: {
+        lightMode: {
+          light: "#fbf5e9",
+          lightgray: "#eadcc8",
+          gray: "#a99577",
+          darkgray: "#4d3c2a",
+          dark: "#24190f",
+          secondary: "#7b2d1f",
+          tertiary: "#2d5c4d",
+          highlight: "rgba(123, 45, 31, 0.12)",
+          textHighlight: "#f1d86a88",
+        },
+        darkMode: {
+          light: "#15120e",
+          lightgray: "#35291f",
+          gray: "#8b765d",
+          darkgray: "#e6d5bc",
+          dark: "#fff3df",
+          secondary: "#d98a6a",
+          tertiary: "#9cc7a9",
+          highlight: "rgba(217, 138, 106, 0.16)",
+          textHighlight: "#9f7d2388",
+        },
+      },
+    },
+  },
+  plugins: {
+    transformers: [
+      Plugin.FrontMatter(),
+      Plugin.CreatedModifiedDate({
+        priority: ["frontmatter", "filesystem"],
+      }),
+      Plugin.SyntaxHighlighting({
+        theme: {
+          light: "github-light",
+          dark: "github-dark",
+        },
+        keepBackground: false,
+      }),
+      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
+      Plugin.GitHubFlavoredMarkdown(),
+      Plugin.TableOfContents(),
+      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+      Plugin.Description(),
+      Plugin.Latex({ renderEngine: "katex" }),
+    ],
+    filters: [Plugin.RemoveDrafts()],
+    emitters: [
+      Plugin.AliasRedirects(),
+      Plugin.ComponentResources(),
+      Plugin.ContentPage(),
+      Plugin.FolderPage(),
+      Plugin.TagPage(),
+      Plugin.ContentIndex({
+        enableSiteMap: true,
+        enableRSS: true,
+      }),
+      Plugin.Assets(),
+      Plugin.Static(),
+      Plugin.Favicon(),
+      Plugin.NotFoundPage(),
+      // Comment out CustomOgImages to speed up build time
+      Plugin.CustomOgImages(),
+    ],
+  },
+}
+
+export default config
