4afe18d49224ed56bdf42a2640b0d09611ecb18c
alzyras <tatiminskas@gmail.com>
2026-05-07 19:09:30 +0300

    Add Bing site verification meta tag

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -52,6 +52,7 @@ export default (() => {
         )}
         <link rel="preconnect" href="https://cdnjs.cloudflare.com" crossOrigin="anonymous" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="msvalidate.01" content="AE02E81D7A17D88CE5D3EB45689C0071" />
 
         <meta name="og:site_name" content={cfg.pageTitle}></meta>
         <meta property="og:title" content={title} />
